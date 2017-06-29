658cd77892b6b548d8c4cefbc133cbe7f47f460a
Sofie Therese Hansen <sofha@win.dtu.dk>
2017-06-29 08:58:46 +0200

    Updated trainModel with comments and moved predefined variables to top

--- functions/trainModel.m
+++ functions/trainModel.m
@@ -1,16 +1,15 @@
 function out=trainModel(options)
 out.options=options;
-%addpath C:\Users\sofha\Documents\realtime\git\brainscanner\functions
-%addpath(genpath('C:\Users\sofha\Documents\realtime\chronux_2_11'))
-% sampling rate
 fs=options.samplingRate;
-% number of channels
-num_channels = 24;
+num_channels = 24;% number of channels
 run_source=input('Run source localization (0/1): ');
 out.run_source=run_source;
 uf=2;% upsample factor
 out.uf=uf;
-run_calcGamma=1;
+run_calcGamma=1; % Set to 0 and specify a gamma value if you have it predefined.
+gamma_predefined=-65; % Set sparsity parameter
+eeg_channel_list=9:10'; % Used if run_source=0;
+out.feature_idx = 5:15; %Hz, frequencies of interest
 %% Load data
 
 num_trials = 1;
@@ -51,57 +50,24 @@ for idx_trial = 1:num_trials
     end
     data{idx_trial} = subject_data(1:num_labelled_samples, 1:num_channels)';
     
-    % how many samples do have labels for?
-%     num_labelled_samples = length(stim_data.stim)*fs;
-%     data{idx_trial} = subject_data(1:num_labelled_samples, 1:num_channels)';
-    %data{idx_trial} = subject_data(:, 1:24)';
-    
-    %fprintf('done.\n\n');
-    
     
 end;
-%% Preprocess
-
-% filter and rereference
-% data_preprocessed = cell(num_trials, 1);
-% 
-% wl=1/uf*fs;
-% for idx_trial = 1:num_trials
-%     for idx_channel = 1:num_channels
-%         for d=1:floor(length(data{idx_trial})/wl);
-%             %if d==1;
-%             %   [data_pre,zf] = filter(filterB, filterA, [data{idx_trial}(idx_channel,(d-1)*wl+1:wl*d),zeros(1,500)]);
-%             [data_preprocessed{idx_trial}(idx_channel,(d-1)*wl+1:wl*d)] = filtfilt(options.experiment.filterB,options.experiment.filterA, [data{idx_trial}(idx_channel,(d-1)*wl+1:wl*d)]);
-%             %else
-%             %  [data_preprocessed{idx_trial}(idx_channel,(d-1)*wl+1:wl*d),zf] = filter(filterB, filterA, data{idx_trial}(idx_channel,(d-1)*wl+1:wl*d),zf);
-%             %end
-%         end
-%     end
-% end;
-% for idx_trial = 1:num_trials
-%     W=eye(num_channels)-1/num_channels;
-%     data_preprocessed{idx_trial} = W*data_preprocessed{idx_trial};
-% end;
-% if isfield(options,'asr_state')
-%     [data_preprocessed{idx_trial} , out.asr_state] = asr_process(data_preprocessed{idx_trial} , fs, options.asr_state);
-% end
 
 %% Apply preprocessing block-wise
-
 wl=1/uf*fs;
 data_preprocessed = zeros(size(data{idx_trial}));
 for d=1:floor(length(data{1})/wl)
     [data_preprocessed(:,(d-1)*wl+1:wl*d)] = preprocess(data{idx_trial}(:,(d-1)*wl+1:wl*d), options.experiment);
 end
 
-%% Artifact removal
+%% Artifact removal using ASR
 if options.experiment.artefactRemoval
     if isfield(options.experiment, 'asr_state')
         [data_preprocessed, out.asr_state] = asr_process(data_preprocessed , fs, options.experiment.asr_state);
     else
         fprintf('trainModel: Load calibration data before applying ASR!\n');
-    end;
-end;
+    end
+end
 
 
 if isfield(options.experiment,'asr_state')
@@ -114,8 +80,6 @@ else
 end
 %% Keep structure
 data_preprocessed = {data_preprocessed};
-
-
 
 %%
 train_trial_idx = 1;
@@ -132,17 +96,19 @@ if run_source==1
     easyCapModelFull=(eye(no_chan)-1/no_chan)*easyCapModelFull; % set gain to have average reference
     forwardModel = easyCapModelFull*basisFunctions';
     %
-    if run_calcGamma==1
+    if run_calcGamma==1 % Estimate the sparsity parameter, gamma
         wl=1/uf*fs;
         disp('Training source localization parameter')
-        opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1;opts.min_gamma=-100;
+        opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1;% Use active set
+        opts.min_gamma=-100;
         for d=1:floor(length(data_preprocessed{idx_trial})/wl)
             if ~rem(d,50)
                 fprintf('window %d out of %d windows\n',d,floor(length(data_preprocessed{idx_trial})/wl))
             end
             [gamma_mean(d), gamma_median(d)]= teVGGD_wcross(forwardModel,data_preprocessed{train_trial_idx}(:,(d-1)*wl+1:wl*d), opts);
         end
-    else gamma_median=-66;
+    else
+        gamma_median=gamma_predefined; % Use predefined gamma
     end
 end
 %%
@@ -150,33 +116,27 @@ if run_source==1
     gamma_median=median(gamma_median);%64.47;%-24.2;
     out.gamma=gamma_median;
     fprintf('Gamma is set to %d\n',gamma_median)
-    opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1;
+    opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1; % Use active set
     wl=1/uf*fs;
-    Sources = cell(num_trials, 1);AllS = cell(num_trials, 1);
+    Sources = cell(num_trials, 1);
     opts.m0=zeros(569,1);
     for idx_trial = 1:num_trials
         fprintf('Calculating sources\n');
         for d=1:floor(length(data_preprocessed{idx_trial})/wl);
-            [sources,msources,~,~] = teVGGD(forwardModel,data_preprocessed{idx_trial}(:,(d-1)*wl+1:wl*d),gamma_median,opts);
-            %AllS{idx_trial}(:,(d-1)*wl+1:wl*d)=basisFunctions'*sources;
+            [sources,~,~,~] = teVGGD(forwardModel,data_preprocessed{idx_trial}(:,(d-1)*wl+1:wl*d),gamma_median,opts);
             Sources{idx_trial}(:,(d-1)*wl+1:wl*d)=sources;
         end
     end
 end
 
-%%
+%% If source localization is chosen then find the sources (basisfunctions) that are most correlated with the stimulation
+
 if run_source==1
-    stim1=repmat(stims{train_trial_idx},[1,125])';
+    stim1=repmat(stims{train_trial_idx},[1,fs/uf])'; % repeat stimuli to match the EEG windows
     stim1=stim1(:);
-    % figure,plot(stim1);
-    % n=16;%6 9;
-    % act=find(sum(Sources{train_trial_idx},2));
-    % hold on,plot(Sources{1}(act(n),:)/max(abs(Sources{1}(act(n),:))))
     clear co
-    for c=1:569%
+    for c=1:size(basisFunctions,1)%
         [up,lo]=envelope(Sources{train_trial_idx}(c,:));%AllS{1}%data_preprocessed
-        %[up,lo]=envelope(AllS{1}(c,:));%%data_preprocessed
-        
         tmp=corrcoef(up,stim1');
         co(c)=tmp(2);
     end
@@ -192,20 +152,19 @@ if run_source==1
     legend('Source with maximum correlation to stimulation','Stimulation')
     out.BFcorr=j;
 end
-%%
+%% Run source localization again where the support is initialiazed to be active in the two most correlated sources.
 if run_source==1
     gamma_median=median(gamma_median);%64.47;%-24.2;
     out.gamma=gamma_median;
     fprintf('Gamma is set to %d\n',gamma_median)
-    opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1;
+    opts.run_prune=1;opts.prune=1e-2;opts.pnorm = 1;% Use active set
     wl=1/uf*fs;
-    Sources = cell(num_trials, 1);AllS = cell(num_trials, 1);
+    Sources = cell(num_trials, 1);
     opts.m0=zeros(569,1);opts.m0(j(1:2),1)=1;
     for idx_trial = 1:num_trials
         fprintf('Calculating sources again\n');
         for d=1:floor(length(data_preprocessed{idx_trial})/wl);
-            [sources,msources,~,~] = teVGGD(forwardModel,data_preprocessed{idx_trial}(:,(d-1)*wl+1:wl*d),gamma_median,opts);
-            %AllS{idx_trial}(:,(d-1)*wl+1:wl*d)=basisFunctions'*sources;
+            [sources,~,~,~] = teVGGD(forwardModel,data_preprocessed{idx_trial}(:,(d-1)*wl+1:wl*d),gamma_median,opts);
             Sources{idx_trial}(:,(d-1)*wl+1:wl*d)=sources;
         end
     end
@@ -214,35 +173,20 @@ end
 %% Extract features
 if run_source==1
     for tr=1:num_trials
-        %[up,lu]=envelope(AllS{tr}(j(1:100),:).^2);
         y{tr}=((Sources{tr}(j(1:50),:)).^2);%
-        %y{tr}=sum(AllS{tr}(occ,:).^2);%
-        out.channel_list = 1:2;%:10;%10;%9:10';%;occ(361);%1;jh(1)';%find(sum(Sources{train_trial_idx},2))';%[1:45];
+        out.channel_list = 1:2;
         
     end
 else
     y=data_preprocessed;
-    out.channel_list=9:10';
+    out.channel_list=eeg_channel_list; % Change
 end
 features = cell(num_trials, 1);
-
-% which channels to include
-
-%channel_list = 1:5;%10;%9:10';%;occ(361);%1;jh(1)';%find(sum(Sources{train_trial_idx},2))';%[1:45];
-
-% which frequencies to include?
-out.feature_idx = 5:15;
-
 figure;
-
 for idx_trial = 1:num_trials
-    
     fprintf('Extracting features for trial %d...', idx_trial);
-    
-    feat = [];
-    
+    feat = [];   
     for idx_channel = out.channel_list
-        idx_channel
         % compute multitaper spectrogram
         params = struct('Fs', fs, 'tapers', [2, 9]); % timebandwidth product = 2, num tapers = 9
         movingwin = [1/uf, 1/uf]; % window size and window step
@@ -254,10 +198,8 @@ for idx_trial = 1:num_trials
             [up((d-1)*wl+1:wl*d),lo]=envelope(y{idx_trial}(idx_channel, (d-1)*wl+1:wl*d));
         end
         [S,t, f] = mtspecgramc(up, movingwin, params );
-        %S(S==-Inf)=[];%S(1);
         feat = [feat log10(S(:, out.feature_idx))];
-        %feat(feat==-Inf)=feat(1);
-    end;
+    end
     
     % extract
     features{idx_trial} = feat;
@@ -278,8 +220,6 @@ out.features=features;
 fprintf('\n\n')
 
 %% standardize before fitting model
-
-
 m = nanmean(features{train_trial_idx}, 1);
 s = nanstd(features{train_trial_idx}, 1);
 for idx_trial = 1:num_trials
@@ -295,14 +235,13 @@ train_stim=stims{train_trial_idx};
 excl=1*uf; % number of trials to exclude on both sides of condition transition;
 trans=find(diff(train_stim));
 trans=repmat(trans,[1,excl*2])+repmat(-excl:excl-1,[length(trans),1]);
-train_feat(trans,:)=[];
-train_stim(trans)=[];
+%train_feat(trans,:)=[];
+%train_stim(trans)=[];
 
 % train
-%model = fitcdiscr(features{train_trial_idx}, stims{train_trial_idx}, 'DiscrimType','Linear');
-%model = fitcdiscr(features{train_trial_idx}, stims{train_trial_idx}, 'DiscrimType','Quadratic')
 model = fitglm(features{train_trial_idx}, stims{train_trial_idx},'linear','Distribution','binomial', 'Intercept', true);
-%model = fitglm(train_feat, train_stim,'linear','Distribution','binomial', 'Intercept', true);
+%model = fitglm(train_feat, train_stim,'linear','Distribution','binomial',
+%'Intercept', true); % Use to discard data around task switches
 out.model=model;
 
 fprintf('done.\n')
@@ -312,7 +251,7 @@ fprintf('Predicting...')
 predicted_stim = cell(num_trials, 1);
 for idx_trial = 1:num_trials
     predicted_stim{idx_trial} = predict(model, features{idx_trial});
-end;
+end
 fprintf('done.\n\n')
 
 % map from probabilities to labels
@@ -322,26 +261,20 @@ binarize = @(x) 1.0*(x > 0.5);
 classification_acc = zeros(num_trials, 1);
 for idx_trial = 1:num_trials
     classification_acc(idx_trial) = mean(stims{idx_trial}(:) == binarize(predicted_stim{idx_trial}(:)));
-    if idx_trial == train_trial_idx; traintest = 'training data'; else;  traintest = 'test data'; end;
-    
+    if idx_trial == train_trial_idx; traintest = 'training data'; else;  traintest = 'test data'; end   
     % output
     fprintf('Trial %d: Acc = %4.3f (%s)\n', idx_trial, classification_acc(idx_trial), traintest)
-end;
+end
 out.trainingError=classification_acc(idx_trial);
 %% Plot
 
 figure;
-for idx_trial = 1:num_trials
-    
-    if idx_trial == train_trial_idx; traintest = 'training data'; else;  traintest = 'test data'; end;
-    
-    hold on;
-    
+for idx_trial = 1:num_trials    
+    hold on;  
     plot(stims{idx_trial}, 'k-', 'linewidth', 3)
     plot(predicted_stim{idx_trial}, 'r--')
     legend('Stimuli', 'Prediction')
     ylim([-0.1, 1.1]);
     title(sprintf('Trial %d', idx_trial));
-    grid on;
-    
-end;
+    grid on;  
+end
